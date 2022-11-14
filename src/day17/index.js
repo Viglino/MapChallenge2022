2a5b3eb9a7316274cd716b0a3836b3e0071119b4
viglino <jean-marc.viglino@ign.fr>
2022-11-14 14:22:15 +0100

    [BUILD]

--- src/day17/index.js
+++ src/day17/index.js
@@ -9,10 +9,26 @@ import info from './page-info.html'
 import './index.css'
 setInfo(info)
 
-map.getView().setZoom(9)
-map.getView().setCenter([259798, 5931906])
+map.getView().setZoom(12)
+map.getView().setCenter([667601, 5706097])
+map.getView().setRotation(-1.065)
 
 const backLayer = new ol_layer_Geoportail({ 
   layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2'
 })
 map.addLayer(backLayer)
+
+import GeoImageLayer from 'ol-ext/layer/GeoImage'
+import GeoImageSource from 'ol-ext/source/GeoImage'
+var geoimg = new GeoImageLayer({
+  title: 'Carte du Pays compris entre les Bauges, Barraux, Montmellian et Chambery',
+  source: new GeoImageSource({
+    "url": "https://viglino.github.io/MapChallenge2022/chambery_bnf.jpg",
+    "imageCenter": [667882.4381152793,5706259.470774345],
+    "imageRotate": 1.0648505348326442,
+    "imageScale": [6.40034504205837,6.40034504205837],
+    "imageMask": [[650647.9243326829,5714446.888497483],[665514.2663181411,5687110.424122906],[685202.0902688706,5697955.305616069],[670286.8458426706,5725242.867549903],[650647.9243326829,5714446.888497483]],
+    attributions: [ "<a href='https://gallica.bnf.fr/ark:/12148/btv1b55013142z/f1.item'>&copy; BnF</a>" ]
+  })
+})
+map.addLayer(geoimg)
