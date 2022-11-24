3e05baa83c3308dd7cb42887a432557d7c8bf1d0
viglino <jean-marc.viglino@ign.fr>
2022-11-24 13:35:07 +0100

    [ADD] day 28

--- src/common/index.js
+++ src/common/index.js
@@ -1,1 +1,22 @@
+import ol_ext_element from 'ol-ext/util/element'
+
 import './style.css'
+
+const galery = document.querySelector('ul.galery')
+
+document.querySelectorAll('ul.list a').forEach(a => {
+  const li = ol_ext_element.create('LI', {
+    html: ol_ext_element.create('IMG', {
+      src: a.href.replace(/html$/, 'png'),
+      title: a.innerText,
+      click: () => {
+        window.location = a.href
+      }
+    }),
+    parent: galery
+  })
+  ol_ext_element.create('LABEL', {
+    text: a.innerText,
+    parent: li
+  })
+})
---
+++ src/day28/index.js
@@ -0,0 +1,46 @@
+import ol_layer_Geoportail from 'ol-ext/layer/Geoportail.js'
+import Mask from 'ol-ext/filter/Mask'
+import Fill from 'ol/style/Fill.js'
+import map from '../common/map.js'
+
+import setInfo from '../common/setInfo.js'
+import info from './page-info.html'
+
+import MultiPolygon from 'ol/geom/MultiPolygon'
+import Feature from 'ol/Feature'
+
+import './index.css'
+setInfo(info)
+map.getView().setZoom(17.2)
+map.getView().setCenter([-187893, 6120701])
+
+const backLayer = new ol_layer_Geoportail({ layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2' })
+map.addLayer(backLayer)
+
+import TileWFS from 'ol-ext/source/TileWFS'
+import VectorLayer from 'ol/layer/Vector.js'
+var source = new TileWFS({
+  url: 'https://wxs.ign.fr/essentiels/geoportail/wfs',
+  typeName: 'BDTOPO_V3:batiment',
+  tileZoom: 16
+});
+const bati = new VectorLayer({
+  title: 'Buildings',
+  source: source,
+  maxResolution: 3,  // prevent load on small zoom 
+  declutter: true
+})
+map.addLayer(bati)
+
+// Add a render 3D to the buildings
+import render3D from 'ol-ext/layer/Render3D'
+var r3D = new render3D({ 
+  height: function(f) {
+    return f.get('hauteur') / 1.5;
+  }, 
+  //ghost: true,
+  active: true,
+  maxResolution: 2, 
+  defaultHeight: 3.5 
+});
+bati.setRender3D(r3D);
